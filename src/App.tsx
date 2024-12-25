9da30d205e15afdf00846bf3d594331ce7e82d3e
Hong Yue Wang <hongyuew26@gmail.com>
2024-12-24 21:17:45 -0500

    Add useState event listener instead

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { AudioClip } from './types';
-import Drum from './Drum';
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { AudioClip } from "./types";
+import Drum from "./Drum";
 
 const _1_korg_minipops: AudioClip[] = [
   {
     name: "wood",
     keyTrigger: "Q",
-    address: "../samples/Korg_Minipops/wood1.wav"
+    address: "../samples/Korg_Minipops/wood1.wav",
   },
   {
     name: "tom",
     keyTrigger: "W",
-    address: "../samples/Korg_Minipops/tom2.wav"
+    address: "../samples/Korg_Minipops/tom2.wav",
   },
   {
     name: "hi-hat",
     keyTrigger: "A",
-    address: "../samples/Korg_Minipops/hihat2.wav"
+    address: "../samples/Korg_Minipops/hihat2.wav",
   },
   {
-    name: "snare drum",
+    name: "snare",
     keyTrigger: "S",
-    address: "../samples/Korg_Minipops/sd2.wav"
+    address: "../samples/Korg_Minipops/sd2.wav",
   },
   {
-    name: "bass drum",
+    name: "bass",
     keyTrigger: "D",
-    address: "../samples/Korg_Minipops/bd2.wav"
-  }
+    address: "../samples/Korg_Minipops/bd2.wav",
+  },
 ];
 
 let audioClips = _1_korg_minipops;
 
 function App() {
-  const playAudio = (e: React.KeyboardEvent<HTMLDivElement>) => {
-    const clip = audioClips.find((clip) => clip.keyTrigger === e.key.toUpperCase());
+  const playAudio = (e: KeyboardEvent) => {
+    const clip = audioClips.find(
+      (clip) => clip.keyTrigger === e.key.toUpperCase()
+    );
     if (!clip) return;
-    (document.getElementById(clip.keyTrigger) as HTMLAudioElement)?.play().catch(console.error);
+    (document.getElementById(clip.keyTrigger) as HTMLAudioElement)
+      ?.play()
+      .catch(console.error);
     document.getElementById("drum-" + clip.keyTrigger)?.focus();
-  }
+  };
 
-  return (<div className="container" id="drum-machine" onKeyDown={playAudio}>
-    <h1>drum machine</h1>
-    <div className="whole-drum">
-      {audioClips.map((clip) => <Drum audioClip={clip} key={clip.keyTrigger}/>)}
+  useEffect(() => {
+    document.addEventListener("keydown", (e) => playAudio(e));
+    return () => document.removeEventListener("keydown", (e) => playAudio(e));
+  });
+
+  return (
+    <div className="container" id="drum-machine">
+      <h1>drum machine</h1>
+      <div className="whole-drum">
+        {audioClips.map((clip) => (
+          <Drum audioClip={clip} key={clip.keyTrigger} />
+        ))}
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default App
+export default App;
